c3728b5d5f73a5bf3846cd11ad83017e2c83ed2c
yuriprym <samuel.young.103@gmail.com>
2020-12-01 19:58:11 -0600

    adding in 2020 and 2019 work

---
+++ 2020/java_solution/src/Advent/DayOne.java
@@ -0,0 +1,24 @@
+package Advent;
+
+public class DayOne {
+
+	int Addition(int sum , int data) {
+		return sum + data;
+}
+	int Multiplication(int sum, int data) {
+		return data *sum ;
+	}
+	boolean EqualsTwentyTwenty(int sum) {
+		if (sum == 2020) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	int Additon(int sum, int data, int add) {
+		return sum + data + add;
+	}
+	int multiplication(int sum, int data, int add) {
+		return sum * data * add;
+	}
+}
